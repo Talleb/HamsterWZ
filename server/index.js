7d4e1d51a626c6a32dfb7f975ac97765e936b79e
Talleb <taulant_09_@hotmail.com>
2021-06-27 17:18:48 +0200

    changed static route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.use(express.static(__dirname + '/../build'));
-app.use('/assets', express.static(__dirname + '/hamsters'));
+app.use('/hamsters', express.static(__dirname + '/hamsters'));
 
 
 
